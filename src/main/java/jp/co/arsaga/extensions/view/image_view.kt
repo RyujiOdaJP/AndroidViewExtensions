d2add040755c5b39b332fb5674168b3f69d4e563
内田正輝 <masaki.u@arsaga.jp>
2020-07-09 14:37:18 +0900

    BindingAdapterの名前を型が明示的なものに変更

--- src/main/java/jp/co/arsaga/extensions/view/image_view.kt
+++ src/main/java/jp/co/arsaga/extensions/view/image_view.kt
@@ -6,13 +6,13 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("android:src")
+@BindingAdapter("binding_url")
 fun ImageView.imageUrl(imageUrl: String?) = imageUrl?.let { Glide.with(context).load(it).into(this) }
 
-@BindingAdapter("android:src")
+@BindingAdapter("binding_resource")
 fun ImageView.resourceId(resourceId: Int?) = resourceId?.let { Glide.with(context).load(it).into(this) }
 
-@BindingAdapter("android:src")
+@BindingAdapter("binding_drawable")
 fun ImageView.imageDrawable(drawable: Drawable?) = drawable?.let { Glide.with(context).load(it).into(this) }
 
 @BindingAdapter("binding_bitmap")
