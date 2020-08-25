afd7208ea84f10a666e3ca5dfa6803a61f634d5a
内田正輝 <masaki.u@arsaga.jp>
2020-08-25 10:49:08 +0900

    DataBindingを使ったリサイクラービューのアダプターを作成

--- src/main/java/jp/co/arsaga/extensions/view/recycler_view.kt
+++ src/main/java/jp/co/arsaga/extensions/view/recycler_view.kt
@@ -1,6 +1,9 @@
 package jp.co.arsaga.extensions.view
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.databinding.BindingAdapter
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
@@ -15,4 +18,23 @@ fun RecyclerView.listObserve(collection: Collection<Any>?) {
 @BindingAdapter("binding_listDiff")
 fun RecyclerView.listDiff(diffResult: DiffUtil.DiffResult) {
     adapter?.run { diffResult.dispatchUpdatesTo(this) }
+}
+
+class BindingViewHolder<T : ViewDataBinding>(val binding: T) : RecyclerView.ViewHolder(binding.root)
+
+abstract class DataBindingAdapter<T : ViewDataBinding> : RecyclerView.Adapter<BindingViewHolder<T>>() {
+    abstract fun onCreateViewDataBinding(layoutInflater: LayoutInflater, parent: ViewGroup, viewType: Int): T
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): BindingViewHolder<T> = LayoutInflater.from(parent.context)
+        .run { onCreateViewDataBinding(this, parent, viewType) }
+        .run { BindingViewHolder(this) }
+
+    abstract fun onBindViewDataBinding(binding: T, position: Int)
+
+    override fun onBindViewHolder(holder: BindingViewHolder<T>, position: Int) {
+        onBindViewDataBinding(holder.binding, position)
+    }
 }
