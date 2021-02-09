bb364799d7fb0bd021b4c4746a08a2c05cdc6bfa
Masaki-U <42033804+Masaki-U@users.noreply.github.com>
2021-02-09 14:58:36 +0900

    XML上でImageViewの画像を整形するBindingAdapterを作成 (#32)

--- src/main/java/jp/co/arsaga/extensions/view/ImageView.kt
+++ src/main/java/jp/co/arsaga/extensions/view/ImageView.kt
@@ -2,19 +2,58 @@ package jp.co.arsaga.extensions.view
 
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
+import android.util.Log
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.request.target.BitmapImageViewTarget
 
-@BindingAdapter("binding_url")
-fun ImageView.imageUrl(imageUrl: String?) = imageUrl?.let { Glide.with(context).load(it).into(this) }
+@BindingAdapter("binding_url", "binding_shapingType", "binding_shapingParam", requireAll = false)
+fun ImageView.imageUrl(imageUrl: String?, shapingType: ShapingType?, shapingParam: Float?) = imageUrl?.let { Glide.with(context).asBitmap().load(it).attachImage(this, shapingType, shapingParam) }
 
-@BindingAdapter("binding_resource")
-fun ImageView.resourceId(resourceId: Int?) = resourceId?.let { Glide.with(context).load(it).into(this) }
+@BindingAdapter("binding_resource", "binding_shapingType", "binding_shapingParam", requireAll = false)
+fun ImageView.resourceId(resourceId: Int?, shapingType: ShapingType?, shapingParam: Float?) = resourceId?.let { Glide.with(context).asBitmap().load(it).attachImage(this, shapingType, shapingParam) }
 
-@BindingAdapter("binding_drawable")
-fun ImageView.imageDrawable(drawable: Drawable?) = drawable?.let { Glide.with(context).load(it).into(this) }
+@BindingAdapter("binding_drawable", "binding_shapingType", "binding_shapingParam", requireAll = false)
+fun ImageView.imageDrawable(drawable: Drawable?, shapingType: ShapingType?, shapingParam: Float?) = drawable?.let { Glide.with(context).asBitmap().load(it).attachImage(this, shapingType, shapingParam) }
 
-@BindingAdapter("binding_bitmap")
-fun ImageView.imageDrawable(bitmap: Bitmap?) = bitmap?.let { Glide.with(context).load(it).into(this) }
+@BindingAdapter("binding_bitmap", "binding_shapingType", "binding_shapingParam", requireAll = false)
+fun ImageView.imageDrawable(bitmap: Bitmap?, shapingType: ShapingType?, shapingParam: Float?) = bitmap?.let { Glide.with(context).asBitmap().load(it).attachImage(this, shapingType, shapingParam) }
 
+private fun RequestBuilder<Bitmap>.attachImage(
+    imageView: ImageView,
+    shapingType: ShapingType?,
+    shapingParam: Float?
+) = if (shapingType == null) into(imageView)
+else shapingType.shaper(imageView, shapingParam)?.run { into(this) }
+
+enum class ShapingType(val shaper: (ImageView, Float?) -> BitmapImageViewTarget?) {
+    CIRCLE({ imageView: ImageView, _: Float? ->
+        object:BitmapImageViewTarget(imageView){
+            override fun setResource(resource:Bitmap?){
+                RoundedBitmapDrawableFactory
+                    .create(imageView.context.resources, resource).apply {
+                        isCircular = true
+                    }.run {
+                        imageView.setImageDrawable(this)
+                    }
+            }
+        }
+    }),
+    ROUND({ imageView: ImageView, float: Float? ->
+        object:BitmapImageViewTarget(imageView){
+            override fun setResource(resource:Bitmap?){
+                float ?: return
+                RoundedBitmapDrawableFactory
+                    .create(imageView.context.resources, resource).apply {
+                        cornerRadius = float
+                        setAntiAlias(true)
+                    }.run {
+                        imageView.setImageDrawable(this)
+                    }
+            }
+        }
+    })
+}
