460ed97341247262215190f952ed343a605001bc
Ryuji Oda <59087018+RyujiOdaJP@users.noreply.github.com>
2021-07-28 12:53:56 +0900

    ユーザが設定したFontサイズを無効にする機能 (#51)
    * add: bindingAdapter for webTo
    
    * wip: set header with intent
    
    * add: guard expression if url is null
    
    * rm: extra code
    
    * add: args for behavior on null
    
    * add: import
    
    * add: header on binding_url
    
    * fix: argument from Pair to GlideUrl
    
    * refactor
    
    * add: extension to ignore user font settings

--- src/main/java/jp/co/arsaga/extensions/view/TextView.kt
+++ src/main/java/jp/co/arsaga/extensions/view/TextView.kt
@@ -1,11 +1,48 @@
 package jp.co.arsaga.extensions.view
 
+import android.app.Activity
+import android.app.Application
+import android.content.Context.WINDOW_SERVICE
+import android.content.res.Configuration
 import android.graphics.Typeface
+import android.os.Bundle
+import android.view.WindowManager
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.BindingAdapter
 
 
 @BindingAdapter("binding_textStyle")
 fun TextView.textStyle(textStyle: Int?) {
     setTypeface(null, textStyle ?: Typeface.NORMAL)
+}
+
+class IgnoreUserFontScale : Application.ActivityLifecycleCallbacks {
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        ignoreUserFontScale(activity)
+    }
+    override fun onActivityResumed(activity: Activity) {
+        ignoreUserFontScale(activity)
+    }
+    override fun onActivityPaused(activity: Activity) {
+        ignoreUserFontScale(activity)
+    }
+
+    override fun onActivityStarted(activity: Activity) {}
+    override fun onActivityStopped(activity: Activity) {}
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {}
+    override fun onActivityDestroyed(activity: Activity) {}
+
+    private fun ignoreUserFontScale(activity: Activity) {
+        activity.run {
+            val configuration = resources.configuration.also { it.fontScale = 1.0f }
+            val metrics = resources.displayMetrics
+            val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+            wm.defaultDisplay.getMetrics(metrics)
+            metrics.scaledDensity = configuration.fontScale * metrics.density
+            baseContext.resources.updateConfiguration(configuration, metrics)
+        }
+    }
 }
