859c4aaaff45dabcd485d10956187baeab1086b2
内田正輝 <masaki.u@arsaga.jp>
2021-12-01 11:17:24 +0900

    NavDirectionのnavigateをActivityからでも使えるように修正

--- src/main/java/jp/co/arsaga/extensions/view/NavDirections.kt
+++ src/main/java/jp/co/arsaga/extensions/view/NavDirections.kt
@@ -1,9 +1,13 @@
 package jp.co.arsaga.extensions.view
 
+import android.app.Activity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 
 fun NavDirections.navigate(fragment: Fragment, navigateAnimation: NavigateAnimationType? = null) {
     fragment.findNavController().navigate(this, animateNavOptionsFactory(navigateAnimation))
+}
+fun NavDirections.navigate(activity: Activity, navigateAnimation: NavigateAnimationType? = null) {
+    activity.getNavController()?.navigate(this, animateNavOptionsFactory(navigateAnimation))
 }
